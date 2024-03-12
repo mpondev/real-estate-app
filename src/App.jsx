521c7800c16b64c1a8890aa8d9478218d6a0ebc5
Mario Ponce <mario@mpondev.com>
2024-03-12 20:40:39 +0100

    Add react-router, Layout and routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import Homepage from './pages/home/Homepage';
-import Navbar from './components/navbar/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import './layout.scss';
+import Homepage from './pages/home/Homepage';
+import Layout from './pages/layout/Layout';
+import ListPage from './pages/listPage/ListPage';
+import SinglePage from './pages/singlePage/SinglePage';
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <Homepage />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Homepage />,
+        },
+        {
+          path: '/list',
+          element: <ListPage />,
+        },
+        {
+          path: '/:id',
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
