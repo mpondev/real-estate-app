93d32c422c4846078828ee35e762e45ce6b3d11a
Mario Ponce <mario@mpondev.com>
2024-03-12 22:21:31 +0100

    Add ListPage and fake data

--- src/pages/listPage/ListPage.jsx
+++ src/pages/listPage/ListPage.jsx
@@ -1,7 +1,25 @@
+import { listData } from '../../lib/dummyData';
+import Card from '../../components/card/Card';
+import Filter from '../../components/filter/Filter';
+
 import './listPage.scss';
 
 function ListPage() {
-  return <div className="listPage">ListPage</div>;
+  const data = listData;
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map(item => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 }
 
 export default ListPage;
