1c1a410b58477f8ee7c603a68326026b61b91088
mpondev <mario@mpondev.com>
2024-03-13 10:52:19 +0100

    Add SinglePage

--- src/pages/singlePage/SinglePage.jsx
+++ src/pages/singlePage/SinglePage.jsx
@@ -1,7 +1,38 @@
+import Slider from '../../components/slider/Slider';
+import { singlePostData, userData } from '../../lib/dummyData';
+
 import './singlePage.scss';
 
 function SinglePage() {
-  return <div className="singlePage">SinglePage</div>;
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="Pin icon" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="User image" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 }
 
 export default SinglePage;
