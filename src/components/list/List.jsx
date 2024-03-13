05dd6884b573fe6e9e8f3096cf2cab459c40699a
Mario Ponce <mario@mpondev.com>
2024-03-13 19:31:42 +0100

    Add List component for ProfilePage

---
+++ src/components/list/List.jsx
@@ -0,0 +1,16 @@
+import Card from '../card/Card';
+import { listData } from '../../lib/dummyData';
+
+import './list.scss';
+
+function List() {
+  return (
+    <div className="list">
+      {listData.map(item => (
+        <Card key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+
+export default List;
